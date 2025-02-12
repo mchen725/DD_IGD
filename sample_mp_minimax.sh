f5e463424ce980b3612dabbbd96ec466b5c7bd04
mchen725 <131373791+mchen725@users.noreply.github.com>
2025-02-12 22:55:06 +0800

    Update sample_mp_minimax.sh

--- sample_mp_minimax.sh
+++ sample_mp_minimax.sh
@@ -17,7 +17,7 @@ d=6
 
 python sample_mp.py --model DiT-XL/2 --image-size 256 --ckpt path/to/minimax/checkpoint \
     --save-dir "../results/dit-distillation/${spec}-${nsample}-minimax-igd-${cp}-${ntype}-k${k}-gamma${gamma}-r${r}-gi${gi}-low${low}-high${high}" --data-path ../imagenet/train/ \
-    --spec ${spec} --gm-scale ${k} --grad-ipc ${gi} --net-type ${ntype} --depth 10 \
+    --spec ${spec} --gm-scale ${k} --grad-ipc ${gi} --net-type ${ntype} --depth ${d} \
     --low ${low} --high ${high} --ckpt_path ${cp} --repeat ${r} --num-samples ${nsample} --dev-scale ${gamma} \
     --nclass ${nclass} --phase ${phase} --target_nclass ${tart_ncls}
     
