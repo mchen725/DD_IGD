27db9fe36cc790a02909b984a2a04bc72d970442
mchen725 <131373791+mchen725@users.noreply.github.com>
2025-02-12 10:29:49 +0800

    Update sample_mp.sh

--- sample_mp.sh
+++ sample_mp.sh
@@ -18,7 +18,7 @@ d=6
 python sample_mp.py --model DiT-XL/2 --image-size 256 --ckpt pretrained_models/DiT-XL-2-256x256.pt \
     --save-dir "../results/dit-distillation/${spec}-${nsample}-dit-igd-${cp}-${ntype}-k${k}-gamma${gamma}-r${r}-gi${gi}-low${low}-high${high}" --data-path ../imagenet/train/ \
     --spec ${spec} --gm-scale ${k} --grad-ipc ${gi} --net-type ${ntype} --depth 10 \
-    --low ${low} --high ${high} --ckpt_path ${cp} --repeat ${r} --num-samples ${nsample} --lambda-neg ${gamma} \
+    --low ${low} --high ${high} --ckpt_path ${cp} --repeat ${r} --num-samples ${nsample} --dev-scale ${gamma} \
     --nclass ${nclass} --phase ${phase} --target_nclass ${tart_ncls}
     
 
